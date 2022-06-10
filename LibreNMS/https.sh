22944e5273f9000ae14caf5c5e49dd5b44e4e789
KismoZ <kismozcookies@gmail.com>
2022-06-10 18:19:09 +0630

    LibreNMS: Fixes to https script

--- LibreNMS/https.sh
+++ LibreNMS/https.sh
@@ -13,8 +13,12 @@ openssl dhparam -out /etc/nginx/dhparam.pem 4096
 touch /etc/nginx/snippets/self-signed.conf
 touch /etc/nginx/snippets/ssl-params.conf
 
+cat <<EOT >> /etc/nginx/snippets/self-signed.conf
 ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
 ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+EOT
+
+SERVER_NAME=$(grep server_name /etc/nginx/conf.d/librenms.conf)
 
 cp -f "${INSTALL_DIR}"/files/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
 cp -f "${INSTALL_DIR}"/files/librenms_https.conf /etc/nginx/conf.d/librenms.conf
