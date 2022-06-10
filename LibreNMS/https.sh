7b1737b138da4f57e702f5164c2bcf6b337b5497
HAL <kismozcookies@gmail.com>
2022-06-10 16:25:33 +0630

    LibreNMS: SSL Cert Script

---
+++ LibreNMS/https.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+        echo "Script must be run as root user"
+        exit 255
+fi
+
+INSTALL_DIR=$(pwd)
+
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+openssl dhparam -out /etc/nginx/dhparam.pem 4096
+
+touch /etc/nginx/snippets/self-signed.conf
+touch /etc/nginx/snippets/ssl-params.conf
+
+ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+
+cp -f "${INSTALL_DIR}"/files/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
+cp -f "${INSTALL_DIR}"/files/librenms_https.conf /etc/nginx/conf.d/librenms.conf
+
+sed "s/.*server_name.*/${SERVER_NAME}/g" /etc/nginx/conf.d/librenms.conf
+
+systemctl restart nginx
