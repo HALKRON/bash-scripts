131119e683b2845012e5cba8a1c99f8d62fb3969
KismoZ <kismozcookies@gmail.com>
2022-06-12 21:41:36 +0630

    Oxidized: Initialized

---
+++ Oxidized/install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+        echo "Script must be run as root user"
+        exit 255
+fi
+
+add-apt-repository universe
+apt update && apt full-upgrade -y
+apt install ruby ruby-dev libsqlite3-dev libssl-dev pkg-config cmake libssh2-1-dev libicu-dev zlib1g-dev g++ -y
+
+gem install oxidized
+gem install oxidized-script oxidized-web
+
+useradd -s /bin/bash -m oxidized
+
+read -p "Enter LibreNMS Server Address: " -r LIBRE_NMS
+read -p "Enter LibreNMS Token: " -r LIBRE_TOKEN
+
+sudo -u librenms bash << EOF
+oxidized
+
+sed -i "s/LIBRE_NMS/$LIBRE_NMS/g; s/LIBRE_TOKEN/$LIBRE_TOKEN/g" $HOME/.config/oxidized/config
+EOF
+
+
+
