ef60c1edc52812eb70bc988d511f76fbc51ff80f
KismoZ <kismozcookies@gmail.com>
2022-06-13 22:49:00 +0630

    LibreNMS: Small changes to TZ & https.conf

--- LibreNMS/files/alt_datetime.sql
+++ LibreNMS/files/alt_datetime.sql
@@ -1,4 +1,5 @@
 USE librenms;
+SET TIME_ZONE='+00:00';
 ALTER TABLE `notifications` CHANGE `datetime` `datetime` timestamp NOT NULL DEFAULT '1970-01-02 00:00:00' ;
 ALTER TABLE `users` CHANGE `created_at` `created_at` timestamp NOT NULL DEFAULT '1970-01-02 00:00:01' ;
 exit
