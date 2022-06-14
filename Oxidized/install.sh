35ed6f19ac45147e53869d3ecbe315fdbebdbdd6
KismoZ <kismozcookies@gmail.com>
2022-06-14 23:51:12 +0630

    Initial Config for Oxidized

--- Oxidized/install.sh
+++ Oxidized/install.sh
@@ -5,6 +5,8 @@ if [ "$(whoami)" != "root" ]; then
         exit 255
 fi
 
+INSTALL_DIR=$(pwd)
+
 add-apt-repository universe
 apt update && apt full-upgrade -y
 apt install ruby ruby-dev libsqlite3-dev libssl-dev pkg-config cmake libssh2-1-dev libicu-dev zlib1g-dev g++ -y
@@ -17,11 +19,12 @@ useradd -s /bin/bash -m -d /opt/oxidized oxidized
 read -p "Enter LibreNMS Server Address: " -r LIBRE_NMS
 read -p "Enter LibreNMS Token: " -r LIBRE_TOKEN
 
-sudo -u librenms bash << EOF
-oxidized
 #Copy from files to config
 
+sudo -u librenms bash << EOF
+oxidized
+cp -f $INSTALL_DIR/files/oxidized_config $HOME/
 EOF
 
-
+# add service
 
