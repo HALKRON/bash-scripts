60da45e164353c8967f35b494e71135236a73f15
KismoZ <kismozcookies@gmail.com>
2022-07-04 22:26:33 +0630

    Small fixes to LibreNMS Integration Script

--- Oxidized/librenms_integration.sh
+++ Oxidized/librenms_integration.sh
@@ -6,10 +6,10 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 sudo -u librenms bash << EOF
+cd ~
 lnms config:set oxidized.enabled true
 lnms config:set oxidized.url http://127.0.0.1:8888
 lnms config:set oxidized.features.versioning true
 lnms config:set oxidized.group_support true
 lnms config:set oxidized.reload_nodes true
-
 EOF
