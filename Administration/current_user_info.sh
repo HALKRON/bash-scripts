240a8ddbdce016506164e7824783d5df6978431a
ha1kron <77142658+HALKRON@users.noreply.github.com>
2023-11-06 19:55:45 +0100

    A branch for all the unstable or testing scripts

---
+++ Administration/current_user_info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$HOME" || exit
+
+echo -e "\nPrinting the current user"
+whoami && id
+
+echo -e "\nMore details"
+finger "$(whoami)"
+
+echo -e "\nShowing passwd content of the user"
+grep "$(whoami)" /etc/passwd
+
+echo -e "\nShowing the groups the user $(whoami) belongs to"
+grep "$(whoami)" /etc/group
+
